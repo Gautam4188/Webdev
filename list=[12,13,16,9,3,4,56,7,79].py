9f6dd5fa0a19b5adea41b794f87b7e9d1051044c
unknown <gautamsupernatural@gmail.com>
2024-08-07 12:42:38 +0530

    Test Commit

---
+++ list=[12,13,16,9,3,4,56,7,79].py
@@ -0,0 +1,28 @@
+list=[12,13,16,9,3,4,56,7,79]
+ max=list[0]
+
+for i in list
+      if i>max
+           max=i
+print("max is ",max)
+
+
+sum_of_even=0
+sum_of_odd=0
+for i in list:
+    if i%2==0:
+        sum_of_even=sum_of_even+i 
+    else:
+        sum_of_odd=sum_of_odd+i 
+print("sum of even " ,sum_of_even)
+print("sum of odd " ,sum_of_odd)  
+
+
+for j in range(2,num)
+    if num%j==0:
+        count-count+1
+if count==0:
+   print("number is prime " ,num)
+else 
+   print("number is not prime " ,num)   
+
